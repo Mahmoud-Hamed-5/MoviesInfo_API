d05950649b4e84e39abed8639210c80f0d2c0131
Mahmoud Hamed <mahmoud.hamed91.mh@gmail.com>
2022-12-16 19:19:36 +0300

    Setting up Base and Auth Controllers

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,51 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\API\BaseController as BaseController;
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Please Validate', $validator->errors());
+        }
+
+        $input = $request->all();
+
+        $input['password'] = Hash::make($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('My%New%Token%4%Movies%Info')->accessToken;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User registered successfully');
+    }
+
+
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password]))
+        {
+            $user = Auth::user();
+
+            $success['token'] = $user->createToken('My%New%Token%4%Movies%Info')->accessToken;
+            $success['name'] = $user->name;
+            return $this->sendResponse($success, 'User login successfully');
+        }else{
+            return $this->sendError('Please check your email|password', ['error' => 'Unauthorized']);
+        }
+    }
 }
